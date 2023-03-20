b8bc76cc20743c30c50d28046d347ecd55ae56b5
taokepppooo <1075626521@qq.com>
2023-03-20 17:56:43 +0800

    feat: 添加菜单样式

--- src/components/basicMenu/index.vue
+++ src/components/basicMenu/index.vue
@@ -1,7 +1,112 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useNameSpace } from '@/hooks/core/useStyle'
+import type { LayoutMenu } from '@/hooks/layout/useLayoutMenu'
+
+const { prefixCls } = useNameSpace('basic-menu')
+const menu = reactive<LayoutMenu[]>([
+  {
+    title: '1',
+    hasChild: true,
+    children: [
+      {
+        hasChild: false,
+        title: '1-1'
+      }
+    ]
+  },
+  {
+    title: '2',
+    hasChild: true,
+    children: [
+      {
+        hasChild: false,
+        title: '2-1'
+      }
+    ]
+  },
+  {
+    title: '3',
+    hasChild: true,
+    children: [
+      {
+        hasChild: false,
+        title: '3-1'
+      },
+      {
+        hasChild: true,
+        title: '3-2',
+        children: [
+          {
+            hasChild: false,
+            title: '3-2-1'
+          }
+        ]
+      },
+      {
+        hasChild: false,
+        title: '3-3'
+      }
+    ]
+  },
+  {
+    title: '4',
+    hasChild: true,
+    children: [
+      {
+        hasChild: false,
+        title: '4-1'
+      },
+      {
+        hasChild: true,
+        title: '4-2',
+        children: [
+          {
+            hasChild: false,
+            title: '4-2-1'
+          }
+        ]
+      },
+      {
+        hasChild: false,
+        title: '4-3'
+      }
+    ]
+  },
+  {
+    title: '5',
+    hasChild: true,
+    children: [
+      {
+        hasChild: false,
+        title: '5-1'
+      },
+      {
+        hasChild: true,
+        title: '5-2',
+        children: [
+          {
+            hasChild: false,
+            title: '5-2-1'
+          }
+        ]
+      },
+      {
+        hasChild: false,
+        title: '5-3'
+      }
+    ]
+  }
+])
+</script>
 
 <template>
-  <div></div>
+  <ElScrollbar>
+    <ElMenu :class="prefixCls" default-active="1">
+      <LayoutMenuItems :menu="menu"></LayoutMenuItems>
+    </ElMenu>
+  </ElScrollbar>
 </template>
 
-<style lang="less"></style>
+<style lang="less">
+@import url('./src/index.less');
+</style>
--- src/components/layouts/sider/LayoutMenu.vue
+++ src/components/layouts/sider/LayoutMenu.vue
@@ -1,75 +1,7 @@
-<script setup lang="ts">
-import { useNameSpace } from '@/hooks/core/useStyle'
-import type { LayoutMenu } from '@/hooks/layout/useLayoutMenu'
-
-const { prefixCls } = useNameSpace('layout-menu')
-const menu = reactive<LayoutMenu[]>([
-  {
-    title: '1',
-    hasChild: true,
-    children: [
-      {
-        hasChild: false,
-        title: '1-1'
-      }
-    ]
-  },
-  {
-    title: '2',
-    hasChild: true,
-    children: [
-      {
-        hasChild: false,
-        title: '2-1'
-      }
-    ]
-  },
-  {
-    title: '3',
-    hasChild: true,
-    children: [
-      {
-        hasChild: false,
-        title: '3-1'
-      },
-      {
-        hasChild: true,
-        title: '3-2',
-        children: [
-          {
-            hasChild: false,
-            title: '3-2-1'
-          }
-        ]
-      },
-      {
-        hasChild: false,
-        title: '3-3'
-      }
-    ]
-  }
-])
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <ElMenu
-      style="width: 210px"
-      :class="prefixCls"
-      active-text-color="#ffd04b"
-      background-color="#001529"
-      default-active="1"
-      text-color="#fff"
-    >
-      <LayoutMenuItems :menu="menu"></LayoutMenuItems>
-    </ElMenu>
-  </div>
+  <BasicMenu></BasicMenu>
 </template>
 
-<style lang="less">
-@prefix-cls: ~'@{namespace}-layout-menu';
-
-.@{prefix-cls} {
-  height: 100vh;
-}
-</style>
+<style lang="less"></style>
