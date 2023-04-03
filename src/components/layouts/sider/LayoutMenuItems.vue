83ee393bbf4b8dd00b5d23cd9c1f47c0cf1fde96
taokepppooo <1075626521@qq.com>
2023-04-03 14:37:49 +0800

    feat: 添加菜单展开收缩功能

--- src/components/layouts/sider/LayoutMenuItems.vue
+++ src/components/layouts/sider/LayoutMenuItems.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useNameSpace } from '@/hooks/core/useStyle'
 import type { LayoutMenu } from '@/hooks/layout/useLayoutMenu'
 import type { PropType } from 'vue'
 
@@ -7,14 +8,15 @@ const props = defineProps({
 })
 
 const menuSize = unref('18')
+const { prefixCls } = useNameSpace('layout-menu-items')
 </script>
 
 <template>
   <template v-for="(m, _index) in props.menu" :key="_index">
-    <ElSubMenu v-if="m.hasChild" :index="m.title">
+    <ElSubMenu v-if="m.hasChild" :class="prefixCls" :index="m.title">
       <template #title>
-        <icon :size="menuSize" :icon="m.icon" />
-        <span>{{ m.title }}</span>
+        <icon class="icon" :size="menuSize" :icon="m.icon" />
+        <span class="title">{{ m.title }}</span>
       </template>
       <LayoutMenuItems :menu="m.children ?? []"></LayoutMenuItems>
     </ElSubMenu>
@@ -22,4 +24,20 @@ const menuSize = unref('18')
   </template>
 </template>
 
-<style lang="less"></style>
+<style lang="less">
+@prefix-cls: ~'@{namespace}-layout-menu-items';
+@import url('@/assets/mixins/index.less');
+
+.@{prefix-cls} {
+  .title {
+    margin-left: 5px;
+    line-height: 1.5;
+    .ellipsis();
+  }
+
+  .el-icon {
+    height: 1.3em;
+    line-height: 1.3em;
+  }
+}
+</style>
